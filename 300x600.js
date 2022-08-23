3045c75f68059e75e10c89fea74cb823b61c8623
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-08-23 14:50:30 -0300

    Create 300x600.js

---
+++ 300x600.js
@@ -0,0 +1,6 @@
+window.googletag = window.googletag || {cmd: []};
+    	  googletag.cmd.push(function() {
+    	    googletag.defineSlot('/7542/otherSizes', [[160, 600], [300, 250], [300, 100], [120, 600], [300, 600], 'fluid', [120, 240]], '/7542/otherSizes').addService(googletag.pubads());
+    	    googletag.pubads().enableSingleRequest();
+    	    googletag.enableServices();
+    	  });
